04b59604a1d05bb8e2941801c821bbfd76477bd3
Muresan Fineas <muresan.it@gmail.com>
2019-03-04 10:19:36 +0200

    update(home): make home inline componenet
    #5

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,8 +2,12 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  template: `
+    <p>
+      home works!
+    </p>
+  `,
+  styles: []
 })
 export class HomeComponent implements OnInit {
 
